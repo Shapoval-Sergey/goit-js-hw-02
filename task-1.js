218b4405e0edcad0a8f40ea56880357893776d9b
Sergey <shapa92sergey@gmail.com>
2020-04-25 12:04:58 +0300

    Создал js-файлы для выполненния задач

---
+++ task-1.js
@@ -0,0 +1,12 @@
+const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+const logItems = function(array) {
+  console.log(array);
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+logItems(friends);
+logItems(numbers);
